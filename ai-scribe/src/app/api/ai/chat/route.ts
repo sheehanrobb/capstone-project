71ed6b9dac959a867b02e99c40262225330e137b
Sheehan Robb <sheehanrobb01@gmail.com>
2023-07-13 11:19:55 +1200

    basic dicataphone done basic chatbox
    Co-authored-by: Isaac Johnston <isaac@superstruct.nz>

--- ai-scribe/src/app/api/ai/chat/route.ts
+++ ai-scribe/src/app/api/ai/chat/route.ts
@@ -1,17 +1,26 @@
-import { Configuration, OpenAIApi } from 'openai-edge'
+import { Configuration, OpenAIApi } from 'openai'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
- 
+
+//Vercel recommends to run on the edge runtime
+// export const runtime = 'edge'
+
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  organization: 'org-O8mSxa4OReFk9vXjyIO8xmjG',
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  
 })
+
 const openai = new OpenAIApi(config)
  
-export const runtime = 'edge'
+
  
 export async function POST(req: Request) {
+  //extract the prompt from the request body
   const { prompt } = await req.json()
+
+  //request to OPENAI api based on the prompt
   const response = await openai.createCompletion({
-    model: 'gpt-4',
+    model: 'gpt-3.5-turbo',
     stream: true,
     temperature: 0.6,
     prompt: 'What is Next.js?',
--- ai-scribe/src/app/page.tsx
+++ ai-scribe/src/app/page.tsx
@@ -3,6 +3,9 @@ import "regenerator-runtime/runtime.js";
 import Image from 'next/image'
 import styles from './page.module.css'
 import Dictaphone from './components/speech'
+import Chat from "./components/chatBox";
+
+export const runtime = "edge"
 
 export default function Home() {
   return (
@@ -12,6 +15,7 @@ export default function Home() {
           <Dictaphone>
 
           </Dictaphone>
+          <Chat></Chat>
         </div>
         
     </main>
