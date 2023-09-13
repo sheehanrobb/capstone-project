c4ab825a870104de56752317c8e9bc07e867938d
sheehanrobb <sheehanrobb01@gmail.com>
2023-09-13 20:18:17 +1200

    login page and router added

--- ai-scribe/src/app/login/login.tsx
+++ ai-scribe/src/app/login/login.tsx
@@ -1,1 +1,14 @@
+'use client'
+import React from "react"
 
+ export default function Login(){
+
+    return (
+        <div>
+            <div>
+                <img src="CPU-image.jpg"></img>
+            </div>
+
+        </div>
+    )
+}
